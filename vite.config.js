303971f5a00ff8e2f2c5feba3744a7e816645ef2
Shanazar <snizamaddinow@gmail.com>
2024-09-15 22:24:38 +0300

    final changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
+import tailwindcss from 'vite-plugin-tailwindcss';
 
 export default defineConfig({
-  root: './src', 
+  root: './src',
   build: {
     outDir: '../public',
     rollupOptions: {
       input: './src/index.html',
     },
-    publicDir: 'public'
-  }
+    publicDir: 'public',
+  },
+  plugins: [
+    tailwindcss(),
+  ],
 });
